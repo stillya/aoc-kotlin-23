3bdf23cd8e24e60790a4823f20403802d818d6be
stillya <st.ilya.101@gmail.com>
2023-12-02 17:59:10 +0600

    added day 01 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,38 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+	fun part1(input: List<String>): Int {
+		return input.sumOf {
+			(it.first { c -> c.isDigit() }.toString() + it.last { c -> c.isDigit() }
+				.toString()).toInt()
+		}
+	}
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+	/**
+	 * NOTE: "eightwo" -> 82 and not 88
+	 */
+	fun part2(input: List<String>): Int {
+		val digits = mapOf(
+			"one" to "o1e",
+			"two" to "t2o",
+			"three" to "th3ee",
+			"four" to "f4ur",
+			"five" to "f5ve",
+			"six" to "s6x",
+			"seven" to "s7ven",
+			"eight" to "ei8ht",
+			"nine" to "n9ne",
+		)
+		return input.sumOf { i ->
+			var text = i
+			digits.forEach { d ->
+				text = text.replace(d.key, d.value)
+			}
+			 (text.first { c -> c.isDigit() }
+				.toString() + text.last { c -> c.isDigit() }
+				.toString()).toInt()
+		}
+	}
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+	val input = readInput("Day01")
+	part1(input).println()
+	part2(input).println()
 }
